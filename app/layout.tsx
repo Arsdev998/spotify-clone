022170935a2b0541d58a3c6c074707cba3aabc3e
Arsdev998 <arsdev998@gmail.com>
2024-02-10 21:09:14 +0700

    auth and subscription supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Siderbar";
+import SupaBaseProviders from "@/providers/SupaBaseProviders";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupaBaseProviders>
+          <Sidebar>{children}</Sidebar>
+        </SupaBaseProviders>
       </body>
     </html>
   );
