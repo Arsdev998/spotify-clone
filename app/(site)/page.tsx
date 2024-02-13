31640736461fdb978adebdc93fe67f16ec65a03a
Arsdev998 <arsdev998@gmail.com>
2024-02-13 21:30:13 +0700

    add library funsi

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,12 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Heder";
 import ListItem from "@/components/ListItem";
+import { Song } from "@/type";
+import PageContent from "./components/PageContent";
 
-export default function Home() {
+export const revalidate = 0;
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div
       className="
@@ -23,10 +28,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">News Songs</h1>
         </div>
-        <div className="">
-
-          List Of Songs
-        </div>
+       <PageContent songs={songs}/>
       </div>
     </div>
   );
--- components/Library.tsx
+++ components/Library.tsx
@@ -4,7 +4,12 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
-const Library = () => {
+import { Song } from "@/type";
+
+interface LibraryProps {
+  songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({songs}) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -31,7 +36,13 @@ const Library = () => {
           className="text-neutral-400 hover:text-white cursor-pointer transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List Of Songs</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {
+          songs.map((item) => (
+            <div>{item.title}</div>
+          ))
+        }
+        </div>
     </div>
   );
 };
